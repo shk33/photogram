e96bae8ab44561eb2abf238fd9c3392ecb8876bf
Miguel Coronel <m.coronel.seg@gmail.com>
2016-03-01 00:12:32 -0600

    DRYing some controller and template code and add flash messages for CRUD actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only:[:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -9,26 +11,34 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create post_params
-    redirect_to posts_url
+    @post = Post.new post_params
+
+    if @post.save
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created!. Please check the form."
+      render :new
+    end
   end
 
   def show
-    @post = Post.find params[:id]
   end
 
   def edit
-    @post = Post.find params[:id]
   end
 
   def update
-    @post = Post.find params[:id]
-    @post.update post_params
-    redirect_to post_path @post
+    if @post.update post_params
+      flash[:success] = "Post updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed. Please check the form."
+      render :edit
+    end
   end
 
   def destroy
-    @post = Post.find params[:id]
     @post.destroy
     redirect_to posts_url
   end
@@ -36,6 +46,10 @@ class PostsController < ApplicationController
   private
     def post_params
       params.require(:post).permit(:image, :caption)
+    end
+
+    def set_post
+     @post = Post.find params[:id]
     end
 
 end
