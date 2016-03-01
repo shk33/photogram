660b55ed5d48d38785b5e77dfaaf5ad889a11105
Miguel Coronel <m.coronel.seg@gmail.com>
2016-03-01 16:28:27 -0600

    Add user_name attribute and allow to register a user with user_name

--- config/routes.rb
+++ config/routes.rb
@@ -53,8 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root 'posts#index'
-  devise_for :users
   resources :posts
 
 end
