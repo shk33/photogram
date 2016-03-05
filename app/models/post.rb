6adefceac34499e7e1a45f76f7417a30f6e6903f
Miguel Coronel <m.coronel.seg@gmail.com>
2016-03-05 01:01:21 -0600

    Add minimum and maximun number of chars for post caption

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   # Validations
   validates :image,   presence: true
-  validates :caption, presence: true
+  validates :caption, presence: true, length: { minimum: 4, maximum: 300 }
   validates :user_id, presence: true
   has_attached_file :image, styles: { :medium => "640x" }
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
