0c69e226ef6eaea4d0242dc4bb31fcb318046c64
Miguel Coronel <m.coronel.seg@gmail.com>
2016-03-16 23:30:09 -0600

    Follow/Unfollow users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,26 @@ class User < ActiveRecord::Base
   has_many :posts,    dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
+
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
+
   has_attached_file :avatar, styles: { medium: '152x152#' }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   # Because User can like posts
   acts_as_voter
+
+  def follow user_id
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow user_id
+    following_relationships.find_by(following_id: user_id).destroy
+  end
 
   def owns_post? post
     post.user.id == id
@@ -23,5 +38,10 @@ class User < ActiveRecord::Base
 
   def owns_comment? post
     post.user.id == id
+  end
+
+  def is_following? user
+    follow = following_relationships.find_by following_id: user.id
+    Follow.exists? follow
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'relationships/follow_user'
+
+  get 'relationships/unfollow_user'
+
   get 'notifications/link_through'
 
   get 'profiles/show'
@@ -76,6 +80,10 @@ Rails.application.routes.draw do
   #Notifications
   get 'notifications/:id/link_through', to: 'notifications#link_through', as: :link_through
   get 'notifications', to: 'notifications#index'
+
+  # Followers
+  post ':user_name/follow_user',   to: 'relationships#follow_user',   as: :follow_user
+  post ':user_name/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
 
   #Profiles
   get   ':user_name',      to: 'profiles#show',   as: :profile
